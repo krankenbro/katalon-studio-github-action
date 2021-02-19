5035f8f3b76a5d9efbd85c80df6bd5f88f63e694
Alexander Morogov <asmorogov@gmail.com>
2021-02-19 12:10:06 +0200

    Debug

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const user_args = core.getInput("args");
 const xvfbConf = core.getInput("xvfbConfiguration");
 
 try {
-  process.env.DISPLAY=99;
   execute(user_version, "", user_projectPath, user_args, "", "--auto-servernum", { // -n 99 --server-args="-screen 99 1920x1080x24
     info: function (message) {
       console.log(message);
