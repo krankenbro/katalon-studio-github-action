5678d264d128fde77193c40366eeb8dbd7b6e79b
Alexander Morogov <asmorogov@gmail.com>
2021-02-19 10:25:47 +0200

    Fix typo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const xvfbConf = core.getInput("xvfbConfiguration");
 
 try {
   process.env.DISPLAY=99;
-  execute(user_version, "", user_projectPath, user_args, "", '--auto-servernum"', { // -n 99 --server-args="-screen 99 1920x1080x24
+  execute(user_version, "", user_projectPath, user_args, "", "--auto-servernum", { // -n 99 --server-args="-screen 99 1920x1080x24
     info: function (message) {
       console.log(message);
     },
